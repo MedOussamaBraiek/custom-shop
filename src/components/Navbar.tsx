9a0bf47a41c867faa04b4c01b00d884806b0a686
Oussama Braiek <oussemabraiek@gmail.com>
2024-07-14 20:47:44 +0200

    landing page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,16 +2,20 @@ import Link from "next/link";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import { buttonVariants } from "./ui/button";
 import { ArrowRight } from "lucide-react";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-const Navbar = () => {
-  const user = undefined;
-  const isAdmin = false;
+const Navbar = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  const isAdmin = user?.email === process.env.ADMIN_EMAIL;
+
   return (
-    <nav className="sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-/75 backdrop-blur-lg transition-all">
+    <nav className="sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
       <MaxWidthWrapper>
         <div className="flex h-14 items-center justify-between border-b border-zinc-200">
           <Link href="/" className="flex z-40 font-semibold">
-            case <span className="text-green-600">cobra</span>
+            case<span className="text-green-600">cobra</span>
           </Link>
 
           <div className="h-full flex items-center space-x-4">
@@ -28,7 +32,7 @@ const Navbar = () => {
                 </Link>
                 {isAdmin ? (
                   <Link
-                    href="/api/auth/logout"
+                    href="/dashboard"
                     className={buttonVariants({
                       size: "sm",
                       variant: "ghost",
@@ -44,7 +48,7 @@ const Navbar = () => {
                     className: "hidden sm:flex items-center gap-1",
                   })}
                 >
-                  Create Case
+                  Create case
                   <ArrowRight className="ml-1.5 h-5 w-5" />
                 </Link>
               </>
@@ -61,7 +65,7 @@ const Navbar = () => {
                 </Link>
 
                 <Link
-                  href="/api/login"
+                  href="/api/auth/login"
                   className={buttonVariants({
                     size: "sm",
                     variant: "ghost",
@@ -79,7 +83,7 @@ const Navbar = () => {
                     className: "hidden sm:flex items-center gap-1",
                   })}
                 >
-                  Create Case
+                  Create case
                   <ArrowRight className="ml-1.5 h-5 w-5" />
                 </Link>
               </>
