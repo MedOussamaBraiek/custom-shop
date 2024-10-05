e083840083ddd1e0823c5e94ee6fc14b0cd83c8f
Oussama Braiek <oussemabraiek@gmail.com>
2024-10-05 16:55:39 +0200

    jocker version 2

--- src/app/configure/Layout.tsx
+++ src/app/configure/Layout.tsx
@@ -31,6 +31,7 @@ const Layout = ({ children }: { children: ReactNode }) => {
     <MaxWidthWrapper className="flex-1 flex flex-col">
       <ol className="rounded-md bg-white lg:flex lg:rounded-none lg:border-l lg:border-r lg:border-gray-200">
         {STEPS.map((step, i) => {
+          console.log(step);
           const isCurrent = pathname.endsWith(step.url);
           const isCompleted =
             i < STEPS.findIndex((s) => pathname.includes(s.url));
@@ -59,7 +60,7 @@ const Layout = ({ children }: { children: ReactNode }) => {
                 >
                   <span className="flex-shrink-0">
                     <img
-                      src={imgPath}
+                      src={imgPath || "/step-3.png"}
                       className={cn(
                         "flex h-20 w-20 object-contain items-center justify-center",
                         {
--- src/app/configure/design/DesignConfiguration2.jsx
+++ src/app/configure/design/DesignConfiguration2.jsx
@@ -123,7 +123,7 @@ const DesignConfigurator2 = () => {
 
   const defaultSizes = {
     shirt: { width: 400, height: 600 },
-    cup: { width: 450, height: 450 },
+    cup: { width: 400, height: 320 },
     sac: { width: 320, height: 500 },
   };
 
@@ -134,7 +134,7 @@ const DesignConfigurator2 = () => {
 
   const designAreas = {
     shirt: { x: 100, y: 120, width: 200, height: 380 },
-    cup: { x: -60, y: 10, width: 450, height: 310 },
+    cup: { x: -60, y: 10, width: 320, height: 290 },
     sac: { x: -5, y: 50, width: 270, height: 450 },
   };
 
@@ -447,7 +447,7 @@ const DesignConfigurator2 = () => {
 
   return (
     <div className="relative mt-20 grid grid-cols-1 lg:grid-cols-3 mb-20 pb-20">
-      <div className="relative h-[37.5rem] overflow-hidden col-span-2 w-full max-w-4xl flex items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 ">
+      <div className="relative h-[37.5rem] overflow-hidden  w-full max-w-4xl flex items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 lg:col-span-2 col-span-full">
         {/* <TShirtDesigner /> */}
         <div
           style={{
